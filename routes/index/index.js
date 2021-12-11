7e2d99b353e22f36aa28339a9dbac79fb1a801ae
Yangseungchan <uvzone@naver.com>
2021-12-11 11:46:48 +0900

    ejs, jquery, bootstrap 설정

--- routes/index/index.js
+++ routes/index/index.js
@@ -5,21 +5,21 @@ const router = Router();
 const viewPath = path.resolve() + '/views/';
 
 router.get('/', function (req, res) {
-  res.sendFile(viewPath + 'index.html');
+  res.render(viewPath + 'index.html');
 });
 
 router.get('/login', function (req, res) {
-  res.sendFile(viewPath + 'login.html');
+  res.render(viewPath + 'login.html');
 });
 
 router.get('/signup', function (req, res) {
-  res.sendFile(viewPath + 'signup.html');
+  res.render(viewPath + 'signup.html');
 });
 
 router.get('/detail/:id', function (req, res) {
   const { id } = req.params;
   console.log(id);
-  res.sendFile(viewPath + 'detail.html');
+  res.render(viewPath + 'detail.html');
 });
 
 export default router;
