aa4661f0a366f47e3e6daf83fc12716aeb8fc1cc
Yangseungchan <uvzone@naver.com>
2021-12-12 17:12:36 +0900

    cookie 분리해서 보내기

--- routes/index/index.js
+++ routes/index/index.js
@@ -24,8 +24,6 @@ router.get('/', function (req, res) {
     res.redirect('/login');
   }
   res.render(viewPath + 'index.html');
-  //로그인 안되어있으면 로그인 함수로 튕겨
-  //redirect 이용
 });
 
 router.get('/login', (req, res) => {
@@ -38,14 +36,13 @@ router.get('/login', (req, res) => {
 //api to deal with login request
 
 router.get('/signup', function (req, res) {
+  if (req.cookies['UId']) {
+    res.redirect('/');
+  }
   res.render(viewPath + 'signup.html');
-  //여기서 회원가입 코드 파일로 가기
 });
 
 router.get('/detail/:id', function (req, res) {
-  //리스트에서 아이템 별로 순서대로 1 2 3 4 아이디 부여
-  // const { id } = req.params;
-  // console.log(id);
   res.render(viewPath + 'detail.html');
 });
 
